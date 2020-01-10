bdce12de854bbfb45b860929a1bf4404957b2912
Spop08 <future826@hotmail.com>
2020-01-10 11:34:03 +0100

    upload build folder

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import Config from '../config';
-import qs from 'qs'
 
 export default class Login extends Component {
   constructor(props) {
