34f2cec9fe97a4c7c9fcda976356341d1e5ddeb2
Spop08 <future826@hotmail.com>
2020-01-10 11:35:06 +0100

    upload build folder

---
+++ build/precache-manifest.d253e65943222aee08f3b739c920cc19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535c7bfc9ffa9651b7e1c493e6fbc4c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d5738fc763ef0b847fb",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4b1a39a26b3114d50181",
+    "url": "/static/css/main.70d8e105.chunk.css"
+  },
+  {
+    "revision": "1d5738fc763ef0b847fb",
+    "url": "/static/js/2.238cdbb2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.238cdbb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b1a39a26b3114d50181",
+    "url": "/static/js/main.77121735.chunk.js"
+  },
+  {
+    "revision": "10108ba76c6aa131d5ac",
+    "url": "/static/js/runtime-main.db5b7c51.js"
+  }
+]);
